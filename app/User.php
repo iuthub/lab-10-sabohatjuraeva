fbc3d28456670927d225f5eaa34387834fd65b1e
Sabohat <sabohatjuraeva@gmail.com>
2020-04-12 04:06:20 +0500

    Lab 10 Finished

--- app/User.php
+++ app/User.php
@@ -18,6 +18,11 @@ class User extends Authenticatable
         'name', 'email', 'password',
     ];
 
+    public function posts()
+    {
+        return $this->hasMany('App\Post');
+    }
+
     /**
      * The attributes that should be hidden for arrays.
      *
